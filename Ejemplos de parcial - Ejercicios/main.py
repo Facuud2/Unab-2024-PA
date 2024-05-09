6d7406dbe0154b1ffdbd213c16b8537f3316fc4f
DiscardCharge <facundocarri@hotmail.com.ar>
2024-05-09 20:34:18 -0300

    Ejercicios de practica pre 1er parcial

---
+++ Ejemplos de parcial - Ejercicios/main.py
@@ -0,0 +1,5 @@
+import a1erParcialFigGeometrica as figGeo
+# TODO: PROBAR TODAS LAS CLASES QUE SEAN FUNCIONALES
+triangulo1 = figGeo.TrianguloRectangulo(0, 16, 12, 20)
+
+print(triangulo1.perimetro())
